68433faebeaeba59f1ddde2a7e04d59be2e68b9a
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-07 16:19:39 -0500

    corrected missing values variable

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -47,6 +47,7 @@ def main():
         print(entry)
     fig, ax = plt.subplots()
     labels = ["Expenses", "Overages", "Budget"]
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages]
     ax.bar("labels", "values", color=['green', 'red', 'blue'])
     ax.set_title('Your total expenses vs. total budget')
     plt.show()
