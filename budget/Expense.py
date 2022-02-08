3f9bee654d234ef8f9e4f2b1ca52d445f48f3662
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-08 09:05:16 -0500

    Trying to fix bugs

--- budget/Expense.py
+++ budget/Expense.py
@@ -44,10 +44,10 @@ class Expenses():
     def categorize_set_comprehension(self):
 
         necessary_expenses = {x for x in self.list
-            if x.category == "phone" or "Auto and Gas" or "classes" or "Utilities" or "Mortgage":
-            food_expenses = set(categories == "Groceries" or "Eating Out")
-            unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
-            return [necessary_expenses, food_expenses, unnessary_expenses]
+            if x.category == "phone" or x.category == "Auto and Gas" or x.category == "classes" or x.category == "Utilities" or x.category == "Mortgage"}:
+                food_expenses = set(categories == "Groceries" or "Eating Out")
+                unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+                return [necessary_expenses, food_expenses, unnessary_expenses]
 
-        }
+        
 
