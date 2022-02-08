2d1d0b6e50fa61ee475db55242fce46680a70ff6
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-08 13:47:03 -0500

    pus

--- budget/Expense.py
+++ budget/Expense.py
@@ -41,7 +41,7 @@ class Expenses():
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
     
-    def categorize_set_comprehension(self):
+    def categorize_set_comprehension(self, x):
 
         necessary_expenses = {x for x in self.list}
 
