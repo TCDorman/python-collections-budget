72329e48e115eb751bba9ec2265a0dc3a2fa9885
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-08 14:47:43 -0500

    final solution

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -18,37 +18,36 @@ def main():
             print("Sets are NOT equal by subset test")
 
 
-    # timeit.timeit()
-    # print(timeit.timeit(stmt='expenses.categorize_for_loop()',
-    #       setup=
-    #       '''
-    # from . import Expense
-    # expenses = Expense.Expenses()
-    # expenses.read_expenses('data/spending_data.csv')
-    #       ''',
-    #       number=100000,
-    #       globals=globals()
-    #       ))
+    print(timeit.timeit(stmt = "expenses.categorize_for_loop()",
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
+                        number=100000,
+                        globals=globals()))
 
-    # print(timeit.timeit(stmt='expenses.categorize_set_comprehension()',
-    #       setup=
-    #       '''
-    # from . import Expense
-    # expenses = Expense.Expenses()
-    # expenses.read_expenses('data/spending_data.csv')
-    #       ''',
-    #       number=100000,
-    #       globals=globals()
-    #       ))
+    print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()",
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
+                        number=100000,
+                        globals=globals()))
 
-    #     fix, ax = plt.subplots()
-    #     lables = ['Necessary', 'Food', 'Unnecessary']
-    #     divided_expenses_sum = []
-    #     for category_exps in divided_set_comp:
-    #         divided_expenses_sum.append(sum(x.amount for x in category_exps))
+    fig, ax = plt.subplots()
+    labels = ['Necessary', 'Food', 'Unnecessary']
 
-    #     ax.pie(divided_expenses_sum, labels = labels, autopct='%1.1f%%')
-    #     plt.show()
+    divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append( sum(x.amount for x in category_exps) )
+
+    ax.pie(divided_expenses_sum, labels=labels, autopct='%1.1f%%') #, shadow=True, startangle=90)
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
